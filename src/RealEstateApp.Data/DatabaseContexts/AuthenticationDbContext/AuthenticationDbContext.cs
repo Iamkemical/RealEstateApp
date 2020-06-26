76222d072c61280f3873c15f06bad14e9bd89821
Gabriel Isaac <iamkemical1@gmail.com>
2020-06-26 04:15:39 +0100

    added connection strings

--- src/RealEstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/RealEstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace RealEstateApp.Data.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        :base(options)
+        {
+            
+        }
     }
 }
