ce7ae0d68b0a09884f1a71ff0e072f2babc85f72
Gabriel Isaac <iamkemical1@gmail.com>
2020-06-28 07:05:47 +0100

    added models for login/register

--- src/RealEstateApp.Web/Controllers/AccountController.cs
+++ src/RealEstateApp.Web/Controllers/AccountController.cs
@@ -1,17 +1,35 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Web.Models;
 
 namespace RealEstateApp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
