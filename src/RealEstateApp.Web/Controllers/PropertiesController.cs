f6c602d0e4c6dbb40d95ba78f9ebbbd1b8574416
Gabriel Isaac <iamkemical1@gmail.com>
2020-08-26 21:35:24 +0100

    Added Add properties page html to the project

--- src/RealEstateApp.Web/Controllers/PropertiesController.cs
+++ src/RealEstateApp.Web/Controllers/PropertiesController.cs
@@ -1,12 +1,26 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using RealEstateApp.Web.Models;
 
 namespace RealEstateApp.Web.Controllers
 {
     public class PropertiesController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View(); 
         }   
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Add(PropertyModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
